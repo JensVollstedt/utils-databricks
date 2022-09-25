636714ce4bb6c895581b2a751cab92be47076174
Jens Vollstedt <Jvo@inspari.dk>
2022-09-25 19:46:03 +0200

    commit containers

--- src/custom_utils/dp_storage/writer.py
+++ src/custom_utils/dp_storage/writer.py
@@ -9,7 +9,7 @@ def get_destination_path(destination_config: dict) -> str:
     """Extracts destination path from destination_config"""
 
     data_config = list(destination_config.values())[0]
-    mount_point = get_mount_point_name(data_config["account"])
+    mount_point = get_mount_point_name(data_config["account"], data_config["container"])
     destination_path = f'{mount_point}/{data_config["dataset"]}'
 
     return destination_path
@@ -19,7 +19,7 @@ def get_databricks_table_info(destination_config: dict) -> Tuple[str, str]:
     """Constructs database and table names to be used in Databricks."""
 
     data_config = list(destination_config.values())[0]
-    mount_point = get_mount_point_name(data_config["account"])
+    mount_point = get_mount_point_name(data_config["account"], data_config["container"])
     
     database_name = mount_point.split('/')[-1]
     table_name = data_config['dataset']
